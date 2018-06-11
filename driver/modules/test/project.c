026cd1ccdf57fab687190d22478f0ab90b290bd0
dfghjm <popov@ucu.edu.ua>
2018-06-12 00:59:00 +0300

    changed project.c

--- driver/modules/test/project.c
+++ driver/modules/test/project.c
@@ -5,24 +5,35 @@
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 
-static unsigned int gpio17 = 17;
-static bool pinOFF = 0;
-static bool pinON = 1;
+
+
+#define gpio6 = 6;
 
 static int project_init(){
-
     int result = 0;
-    gpio_direction_output(gpio17, pinON);
+    int counter = 0;
+    gpio_request(gpio6, "sysfs");
+    gpio_direction_output(gpio6, 1);
+    mdelay(100);
+    for(counter; counter < 5; counter++){
+    	gpio_set_value(gpio6, 0);
+    	mdelay(100):
+    	gpio_set_value(gpio6, 1);
+    	mdelay(100):
+    }
     return result;
 
 }
 
 static void project_exit(){
 
-    gpio_set_value(gpio17, pinOFF);
-    gpio_free(gpio17);
+    gpio_set_value(gpio6, 0);
+    gpio_free(gpio6);
 
 }
 
 module_init(project_init);
 module_exit(project_exit);
+
+
+MODULE_LICENSE("GPL")
