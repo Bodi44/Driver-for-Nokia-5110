39127fef677f468495f3d995b4624d21266798cf
pavsem <32280258+pavsem@users.noreply.github.com>
2018-06-11 22:21:19 +0000

    halo

--- driver/modules/test/project.c
+++ driver/modules/test/project.c
@@ -1,42 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/semaphore.h>
-#include <linux/uaacess>
-
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
 
 
+#define gpio6 6
 
-struct fake_device
-{
-    char data[100];
-    struct semaphore sem;
-}
-
-
-
-#define gpio6 6;
-
-static int project_init(){
+static int project_init(void){
     int result = 0;
-    int counter = 0;
     gpio_request(gpio6, "sysfs");
     gpio_direction_output(gpio6, 1);
     mdelay(100);
     gpio_set_value(gpio6, 0);
-    mdelay(100):
+    mdelay(100);
     gpio_set_value(gpio6, 1);
     return result;
 
 }
 
-static void project_exit(){
+static void project_exit(void){
 
     gpio_set_value(gpio6, 0);
     gpio_free(gpio6);
@@ -47,6 +31,6 @@ module_init(project_init);
 module_exit(project_exit);
 
 
-MODULE_AUTHOR("Nikita")
+MODULE_AUTHOR("Nikita");
 MODULE_DESCRIPTION(" X Y I ");
 MODULE_LICENSE("GPL");
